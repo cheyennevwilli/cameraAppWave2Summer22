ee866da2378ac110721adbcb705885b262e87222
Cheyenne Vlymen-Williams <cheyennevwilliams@gmail.com>
2022-06-28 15:30:38 -0400

    Initial Commit

---
+++ cameraAppWave2Summer22/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cameraAppWave2Summer22
+//
+//  Created by Cheyenne Vlymen-Williams on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present (imagePicker, animated: true, completion: nil)
+    }
+}
+
